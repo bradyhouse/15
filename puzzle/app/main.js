4ef415ee73f384aa6c84c3de099f30aff2f9dc95
bradyhouse@gmail.com <bradyhouse@gmail.com>
2017-01-23 05:50:16 -0600

    Baseline

--- puzzle/app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_1 = require("nativescript-angular/platform");
-var status_bar_util_1 = require("./utils/status-bar-util");
-var app_module_1 = require("./app.module");
-status_bar_util_1.setStatusBarColors();
-platform_1.platformNativeScriptDynamic().bootstrapModule(app_module_1.AppModule);
